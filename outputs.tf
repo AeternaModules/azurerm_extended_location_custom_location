2723d5c5836f66d003a13dda43fb920d5c4af6be
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:53:48 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "extended_location_custom_locations" {
+  description = "All extended_location_custom_location resources"
+  value       = azurerm_extended_location_custom_location.extended_location_custom_locations
+}
+
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "extended_location_custom_locations" {
+  description = <<EOT
+Map of extended_location_custom_locations, attributes below
+Required:
+    - cluster_extension_ids
+    - host_resource_id
+    - location
+    - name
+    - namespace
+    - resource_group_name
+Optional:
+    - display_name
+    - host_type
+    - authentication (block):
+        - type (optional)
+        - value (required)
+EOT
+
+  type = map(object({
+    cluster_extension_ids = list(string)
+    host_resource_id      = string
+    location              = string
+    name                  = string
+    namespace             = string
+    resource_group_name   = string
+    display_name          = optional(string)
+    host_type             = optional(string)
+    authentication = optional(object({
+      type  = optional(string)
+      value = string
+    }))
+  }))
+}
+
