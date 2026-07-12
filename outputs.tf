484d439187a0f6a6670af3d68a6a46969f0c2c6b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "extended_location_custom_locations" {
-  description = "All extended_location_custom_location resources"
-  value       = azurerm_extended_location_custom_location.extended_location_custom_locations
-}
 output "extended_location_custom_locations_authentication" {
-  description = "List of authentication values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.authentication]
+  description = "Map of authentication values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.authentication }
 }
 output "extended_location_custom_locations_cluster_extension_ids" {
-  description = "List of cluster_extension_ids values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.cluster_extension_ids]
+  description = "Map of cluster_extension_ids values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.cluster_extension_ids }
 }
 output "extended_location_custom_locations_display_name" {
-  description = "List of display_name values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.display_name]
+  description = "Map of display_name values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.display_name }
 }
 output "extended_location_custom_locations_host_resource_id" {
-  description = "List of host_resource_id values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.host_resource_id]
+  description = "Map of host_resource_id values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.host_resource_id }
 }
 output "extended_location_custom_locations_host_type" {
-  description = "List of host_type values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.host_type]
+  description = "Map of host_type values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.host_type }
 }
 output "extended_location_custom_locations_location" {
-  description = "List of location values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.location]
+  description = "Map of location values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.location }
 }
 output "extended_location_custom_locations_name" {
-  description = "List of name values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.name]
+  description = "Map of name values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.name }
 }
 output "extended_location_custom_locations_namespace" {
-  description = "List of namespace values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.namespace]
+  description = "Map of namespace values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.namespace }
 }
 output "extended_location_custom_locations_resource_group_name" {
-  description = "List of resource_group_name values across all extended_location_custom_locations"
-  value       = [for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : v.resource_group_name]
+  description = "Map of resource_group_name values across all extended_location_custom_locations, keyed the same as var.extended_location_custom_locations"
+  value       = { for k, v in azurerm_extended_location_custom_location.extended_location_custom_locations : k => v.resource_group_name }
 }
 
